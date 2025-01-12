6e5fe01a0feaace20f81bc61db48106d7eb5d34d
Alex Hladkykh <a.ablayev@gmail.com>
2025-01-12 02:08:52 +0200

    workaround

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useActionState } from 'react';
+import { useActionState, startTransition } from 'react';
 import { handleSubmit } from './actions';
 import { Form } from '@nextui-org/form';
 import { Input } from '@nextui-org/input';
@@ -9,6 +9,11 @@ export default function Home() {
   const initialState = {input: new FormData(), errors: []}
   const [stateOne, formActionOne] = useActionState(handleSubmit, initialState);
   const [stateTwo, formActionTwo] = useActionState(handleSubmit, initialState);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    startTransition( async () => { await formActionTwo(new FormData(e.target)) });
+  }
 
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
@@ -20,7 +25,7 @@ export default function Home() {
       </div>
 
       <div className="mt-8">
-        <Form action={formActionTwo}>
+        <Form onSubmit={onSubmit}>
           <Input name="name" type="text" defaultValue={stateTwo.input.get('name')} />
           <Button type="submit">Submit</Button>
         </Form>
